34df0e2272d610404fd6d21dcf3ed532f67bfea6
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-12-18 06:45:49 +0530

    Update redirect.routes.ts

--- src/modules/redirect/redirect.routes.ts
+++ src/modules/redirect/redirect.routes.ts
@@ -1,11 +1,24 @@
-import { FastifyInstance } from "fastify";
-import { RedirectController } from "./redirect.controller";
+import { fraudMiddleware } from "../../middlewares/fraud.middleware";
 
-export async function redirectRoutes(app: FastifyInstance) {
+app.get("/:code", async (req, reply) => {
+  const link = await prisma.link.findUnique({
+    where: { code: req.params.code }
+  });
 
-  // Example: https://domain.com/abc123
-  app.get(
-    "/:code",
-    RedirectController.redirect
-  );
-}
+  if (!link) return reply.notFound();
+
+  (req as any).link = link;
+
+  await fraudMiddleware(req, reply);
+
+  // ✅ Legit click
+  await prisma.click.create({
+    data: {
+      linkId: link.id,
+      ip: req.ip,
+      userAgent: req.headers["user-agent"]
+    }
+  });
+
+  return reply.redirect(link.originalUrl);
+});
